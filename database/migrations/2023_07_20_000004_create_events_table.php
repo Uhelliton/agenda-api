ae1dd9a64d66ed18a3ea8519ee7e87fc9b3164d8
Uhelliton Andrade <uhelliton@uol.com.br>
2023-07-21 00:07:35 -0300

    feat(agenda/event): create migrations base

---
+++ database/migrations/2023_07_20_000004_create_events_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->date('start_date');
+            $table->date('due_date');
+            $table->enum('status', ['Aberto', 'Concluido'])->default('Aberto');
+
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id', 'FkEventType')
+                ->references('id')
+                ->on('events_types')
+                ->onUpdate('cascade');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id', 'FkEventUser')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade');
+
+            $table->date('finalization_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
