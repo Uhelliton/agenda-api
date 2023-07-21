257966b82f61fba6431aa19d0b530789f0682cf7
Uhelliton Andrade <uhelliton@uol.com.br>
2023-07-21 01:21:12 -0300

    feat(agenda/request): request validation to create event

---
+++ app/Domains/Agenda/Http/Requests/EventStoreRequest.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Domains\Agenda\Http\Requests;
+
+use App\Support\Rules\ValidDaysRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class EventStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:2',
+            'description' => 'required|min:5',
+            'start_date' => [
+                'required',
+                'date',
+                new ValidDaysRule()
+            ],
+            'due_date' => [
+                'required',
+                'date',
+                'after:start_date',
+                new ValidDaysRule()
+            ],
+            'type_id' => 'required|int',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ], 403));
+    }
+}
