0b1c298c887e1614ac3bedc238d89c2b91dd5cfe
Uhelliton Andrade <uhelliton@uol.com.br>
2023-07-22 03:38:12 -0300

    test(feature/EventE2ETest): test get all agenda events

--- tests/Feature/Agenda/EventE2ETest.php
+++ tests/Feature/Agenda/EventE2ETest.php
@@ -2,18 +2,51 @@
 
 namespace Tests\Feature\Agenda;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Domains\Agenda\Http\Requests\EventStoreRequest;
+use App\Domains\Agenda\Http\Requests\EventUpdateRequest;
+use App\Domains\Agenda\Models\Event;
+use App\Domains\User\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Request;
+use Laravel\Sanctum\Sanctum;
+use Tests\Mockers\EventMocker;
 use Tests\TestCase;
+use Mockery;
+use App\Domains\Agenda\Http\Controllers\EventController;
 
 class EventE2ETest extends TestCase
 {
-//    /**
-//     * A basic test example.
-//     */
-//    public function test_the_application_returns_a_successful_response(): void
-//    {
-//        $response = $this->get('/');
-//
-//        $response->assertStatus(404);
-//    }
+    private $eventRepository;
+
+    public function setUp(): void
+    {
+        parent::setup();
+
+        // create a mock of the post repository interface and inject it into the
+        // IoC container
+        $this->eventRepository = Mockery::mock('App\Domains\Agenda\Repositories\Interfaces\EventRepositoryInterface');
+        $this->app->instance('App\Domains\Agenda\Repositories\Interfaces\EventRepositoryInterface', $this->eventRepository);
+    }
+
+    public function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+
+    public function testIndex()
+    {
+        // simulate auth user
+        Sanctum::actingAs(User::factory()->create());
+
+        $this->eventRepository
+            ->shouldReceive('paginate')
+            ->once()
+            ->andReturn(EventMocker::eventsPaginate());
+
+        $response = $this->getJson('api/agenda/events');
+        $response->assertStatus(200);
+        $response->assertOk();
+        $response->assertJson(EventMocker::eventsPaginate());
+    }
 }
